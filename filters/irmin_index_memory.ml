750ab8e17ad0839341138c14d1021c42efa861e1
Zach Shipko <zachshipko@gmail.com>
2020-12-12 09:02:35 -0800

    Split out reusable code

--- filters/irmin_index_memory.ml
+++ filters/irmin_index_memory.ml
@@ -1,27 +1,4 @@
-open Memtrace.Trace
-
-module Stat = struct
-  type t = {
-    mutable b: float;
-    ids: (Obj_id.t, float) Hashtbl.t
-  }
-
-  let init () = {b = 0.; ids = Hashtbl.create 8}
-
-  let add t id x =
-    t.b <- t.b +. x;
-    Hashtbl.replace t.ids id x
-
-  let remove t id =
-    match Hashtbl.find_opt t.ids id with
-    | Some x ->
-      Hashtbl.remove t.ids id;
-      t.b <- t.b -. x
-    | None -> ()
-
-  let gb t =
-    t.b /. 1024. /. 1024. /. 1024.
-end
+open Memtrace_filter
 
 let print_alloc alloc_time index_size irmin_size =
   let alloc_time = Timedelta.to_int64 alloc_time in
@@ -33,19 +10,6 @@ let print_alloc alloc_time index_size irmin_size =
 
 let irmin_package_name = Str.regexp "Irmin.*"
 let index_package_name = Str.regexp "Index.*"
-
-let nbytes ~trace nsamples =
-  let { Info.sample_rate; word_size; _ } = Reader.info trace in
-  let nwords = Float.of_int nsamples /. sample_rate in
-  nwords *. Float.of_int word_size /. 4.
-
-let is_package_name name trace alloc =
-  Array.exists (fun loc ->
-      let locs = Reader.lookup_location_code trace loc in
-      List.exists (fun loc ->
-          Str.string_match name loc.Location.defname 0
-        ) locs
-    ) alloc
 
 let is_irmin = is_package_name irmin_package_name
 let is_index = is_package_name index_package_name
