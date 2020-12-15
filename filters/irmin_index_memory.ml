dce8b86cc38be1a12bc6f97253338f3aeeed2158
Zach Shipko <zachshipko@gmail.com>
2020-12-14 18:10:15 -0800

    Fix irmin/index comparison

--- filters/irmin_index_memory.ml
+++ filters/irmin_index_memory.ml
@@ -1,37 +1,60 @@
 open Memtrace_filter
 
-let print_alloc alloc_time index_size irmin_size =
+let print_alloc alloc_time index_size irmin_size total_size =
   let alloc_time = Timedelta.to_int64 alloc_time in
-  Printf.printf
-    "timestamp=%f index=%fG irmin=%fG\n"
+  Printf.printf "timestamp=%f index=%fG irmin=%fG total=%fG\n"
     (Int64.to_float alloc_time /. 1_000_000.)
-    (Stat.gb index_size)
-    (Stat.gb irmin_size)
+    (Stat.gb index_size) (Stat.gb irmin_size) (Stat.gb total_size)
 
-let irmin_package_name = Str.regexp "Irmin.*"
-let index_package_name = Str.regexp "Index.*"
+let is_irmin = matches_package_name @@ Str.regexp "Irmin.*"
 
-let is_irmin = is_package_name irmin_package_name
-let is_index = is_package_name index_package_name
+let is_index trace alloc alloc_len =
+  let exception Return of bool in
+  try
+    for i = 0 to alloc_len - 1 do
+      let locs = Reader.lookup_location_code trace alloc.(i) in
+      List.iteri
+        (fun i loc ->
+          let is_index_unix_raw =
+            Str.string_match
+              (Str.regexp "Index_unix__Raw.*")
+              loc.Location.defname 0
+          in
+          let is_actually_irmin_pack_io =
+            i > 0
+            && Str.string_match
+                 (Str.regexp "Irmin_pack__IO.*")
+                 (List.nth locs (i - 1)).defname 0
+          in
+          if is_index_unix_raw && is_actually_irmin_pack_io then
+            raise (Return false)
+          else if Str.string_match (Str.regexp "Index.*") loc.defname 0 then
+            raise (Return true))
+        locs
+    done;
+    false
+  with Return x -> x
 
 let () =
   let trace = Reader.open_ ~filename:Sys.argv.(1) in
   let index_size = Stat.init () in
   let irmin_size = Stat.init () in
+  let total_size = Stat.init () in
   Reader.iter trace (fun time ev ->
       match ev with
       | Alloc alloc ->
-        let size = nbytes ~trace alloc.nsamples in
-        if is_index trace alloc.backtrace_buffer then begin
-          Stat.add index_size alloc.obj_id size;
-          print_alloc time index_size irmin_size
-        end else if is_irmin trace alloc.backtrace_buffer then begin
-          Stat.add irmin_size alloc.obj_id size;
-          print_alloc time index_size irmin_size
-        end
+          let size = nbytes ~trace alloc.nsamples in
+          Stat.add total_size alloc.obj_id size;
+          if is_index trace alloc.backtrace_buffer alloc.backtrace_length then (
+            Stat.add index_size alloc.obj_id size;
+            print_alloc time index_size irmin_size total_size )
+          else if is_irmin trace alloc.backtrace_buffer alloc.backtrace_length
+          then (
+            Stat.add irmin_size alloc.obj_id size;
+            print_alloc time index_size irmin_size total_size )
       | Collect id ->
-        Stat.remove index_size id;
-        Stat.remove irmin_size id
+          Stat.remove total_size id;
+          Stat.remove index_size id;
+          Stat.remove irmin_size id
       | _ -> ());
   Reader.close trace
-  ;
--- lib/memtrace_filter.ml
+++ lib/memtrace_filter.ml
@@ -1,12 +1,9 @@
 include Memtrace.Trace
 
 module Stat = struct
-  type t = {
-    mutable b: float;
-    ids: (Obj_id.t, float) Hashtbl.t
-  }
+  type t = { mutable b : float; ids : (Obj_id.t, float) Hashtbl.t }
 
-  let init () = {b = 0.; ids = Hashtbl.create 8}
+  let init () = { b = 0.; ids = Hashtbl.create 8 }
 
   let add t id x =
     t.b <- t.b +. x;
@@ -15,26 +12,32 @@ module Stat = struct
   let remove t id =
     match Hashtbl.find_opt t.ids id with
     | Some x ->
-      Hashtbl.remove t.ids id;
-      t.b <- t.b -. x
+        Hashtbl.remove t.ids id;
+        t.b <- t.b -. x
     | None -> ()
 
-  let gb t =
-    t.b /. 1024. /. 1024. /. 1024.
+  let gb t = t.b /. 1024. /. 1024. /. 1024.
 end
-
-
 
 let nbytes ~trace nsamples =
   let { Info.sample_rate; word_size; _ } = Reader.info trace in
   let nwords = Float.of_int nsamples /. sample_rate in
   nwords *. Float.of_int word_size /. 4.
 
-let is_package_name name trace alloc =
-  Array.exists (fun loc ->
+let matches_package_name name trace alloc nalloc =
+  Array.exists
+    (fun loc ->
       let locs = Reader.lookup_location_code trace loc in
-      List.exists (fun loc ->
-          Str.string_match name loc.Location.defname 0
-        ) locs
-    ) alloc
+      List.exists (fun loc -> Str.string_match name loc.Location.defname 0) locs)
+    (Array.sub alloc 0 nalloc)
 
+let print_backtrace trace alloc nalloc =
+  for i = 0 to nalloc - 1 do
+    let locs = Reader.lookup_location_code trace alloc.(i) in
+    List.iter
+      (fun loc ->
+        output_string stderr (Location.to_string loc);
+        output_string stderr "\n")
+      locs;
+    flush stderr
+  done
